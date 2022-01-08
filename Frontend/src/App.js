caf6b67ad776fb6be495307adb69e625008ca755
kliff9 <c.thelusca@hotmail.com>
2022-01-08 18:23:06 -0500

    01 update

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -110,7 +110,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<HomeScreen />} exact></Route>
-          <Route path="/cards" element={<DeckScreen />}></Route>
+          {/* <Route path="/mydecks" element={<DeckScreen />}></Route> */}
           <Route path="/mydecks" element={<DeckScreen />}></Route>
           <Route path="/login" element={<LogInScreen />}></Route>
           <Route path="/Signup" element={<SignUpScreen />}></Route>
--- Frontend/src/Screens/HomeScreen.js
+++ Frontend/src/Screens/HomeScreen.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import pic from '../img/ThreePokemonBirds.jpg'
-import './ScreenCSS/HomeScreen.css'
+import pic from "../img/ThreePokemonBirds.jpg";
+import "./ScreenCSS/HomeScreen.css";
 export default function HomeScreen() {
-    return (
-        <React.Fragment>
-            <div>
-                <div className="Home_container">
-                <img src={pic} alt="Picture" className="threebirds"/>
-                  <div className="middlediv">
-                         <h2>Build Your Own Deck Now</h2>                
-                         <Link to="/cards"><button className="bn632-hover">Build</button></Link>
-                     </div>
-                </div>
-
-
-
-            </div>
-
-
-
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div>
+        <div className="Home_container">
+          <img src={pic} alt="Picture" className="threebirds" />
+          <div className="middlediv">
+            <h2>Build Your Own Deck Now</h2>
+            <Link to="/mydecks">
+              <button className="bn632-hover">Build</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
