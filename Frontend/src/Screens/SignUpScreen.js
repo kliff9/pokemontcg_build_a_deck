9bb9cd7a5749834f55c73ab4ca54c672075bb6d8
kliff9 <c.thelusca@hotmail.com>
2022-02-02 10:16:42 -0500

    new

--- Frontend/src/Screens/SignUpScreen.js
+++ Frontend/src/Screens/SignUpScreen.js
@@ -1,26 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './ScreenCSS/LoginScreen.css'
+import React from "react";
+import "./ScreenCSS/LoginScreen.css";
 export default function SignUpScreen() {
+  return (
+    <React.Fragment>
+      <div class="login">
+        <h1>Signup</h1>
+        <form method="post">
+          <input
+            type="text"
+            name="u"
+            placeholder="First Name"
+            required="required"
+          />
+          <input
+            type="text"
+            name="p"
+            placeholder="Last Name"
+            required="required"
+          />
+          <input
+            type="text"
+            name="u"
+            placeholder="Username"
+            required="required"
+          />
+          <input
+            type="password"
+            name="p"
+            placeholder="Password"
+            required="required"
+          />
+          <input
+            type="text"
+            name="p"
+            placeholder="Favorite Pokemon"
+            required="required"
+          />
 
-    return(
-      <React.Fragment>
-
-        <div class="login">
-  <h1>Signup</h1>
-    <form method="post">
-      <input type="text" name="u" placeholder="First Name" required="required" />
-        <input type="text" name="p" placeholder="Last Name" required="required" />
-        <input type="text" name="u" placeholder="Username" required="required" />
-        <input type="password" name="p" placeholder="Password" required="required" />
-        <input type="text" name="p" placeholder="Favorite Pokemon" required="required" />
-
-
-        <button type="submit" class="btn btn-primary btn-block btn-large">Sign Up</button>
-    </form>
-
-</div>
-</React.Fragment>
-
-    )
+          <button type="submit" class="btn btn-primary btn-block btn-large">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </React.Fragment>
+  );
 }
